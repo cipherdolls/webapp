98005216e40479392f7253f383657e7b0bae5f36
ffaerber <ffaerber@gmail.com>
2025-02-20 15:38:56 +0200

    remove test

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -7,22 +7,7 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-
-export async function loader() {
-  const [responseA, responseB] = await Promise.all([
-    fetch("https://jsonplaceholder.typicode.com/todos"),
-    fetch("https://jsonplaceholder.typicode.com/users"),
-  ]);
-  const [dataA, dataB] = await Promise.all([responseA.json(), responseB.json()]);
-  return {
-    dataA,
-    dataB,
-  }
-}
-
-
-
-
+ 
 
 export default function Dashboard() {
   const { dataA, dataB } = useLoaderData();
@@ -31,9 +16,6 @@ export default function Dashboard() {
     <div className="">
         Dashboard
         <div>
-          <h1>Two API Calls in One Loader</h1>
-          <p>Data A: {JSON.stringify(dataA)}</p>
-          <p>Data B: {JSON.stringify(dataB)}</p>
         </div>
     </div>
   );
