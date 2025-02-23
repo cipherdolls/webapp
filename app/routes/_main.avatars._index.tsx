d069f2821d0bfc179be2aaef8b5b66a2fcd4b8ad
ffaerber <ffaerber@gmail.com>
2025-02-23 20:28:22 +0200

    add link to avatar in avatar index

--- app/routes/_main.avatars._index.tsx
+++ app/routes/_main.avatars._index.tsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from "react-router";
+import { Form, Link, redirect } from "react-router";
 import type { Avatar } from "~/types";
 import type { Route } from "./+types/_main.avatars._index";
 
@@ -40,12 +40,12 @@ export default function AvatarsIndex({ loaderData }: Route.ComponentProps) {
 
       {avatars.map((avatar) => (
         <div key={avatar.id} className="flex items-center justify-between">
-          <div>{avatar.name}</div>
+          <Link to={`/avatars/${avatar.id}`}>{avatar.name}</Link>
           
           <Form method='post' action="/chats">
             <input hidden name='avatarId' id='avatarId' value={avatar.id} readOnly />
             <button type='submit' >
-                Start chat
+                Chat
             </button>
           </Form>
 
