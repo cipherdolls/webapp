71d9ae4f8587f889f5bc8dd84ef3f2ad046ceaec
ffaerber <ffaerber@gmail.com>
2025-02-24 12:08:29 +0200

    add chat mqtt client

--- app/routes/_main.chats.$chatId.tsx
+++ app/routes/_main.chats.$chatId.tsx
@@ -1,7 +1,10 @@
 import { Form, Link, Outlet, redirect } from "react-router";
-import type { Chat, Message } from "~/types";
+import type { Chat, Message, ProcessEvent } from "~/types";
 import ChatDestroy from "./chats.$id.destroy";
 import type { Route } from "./+types/_main.chats.$chatId";
+import { useEffect, useRef } from "react";
+import mqtt from 'mqtt';
+import { Buffer } from 'buffer';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -42,6 +45,54 @@ export async function clientLoader({params}: Route.LoaderArgs) {
 
 export default function ChatShow({ loaderData }: Route.ComponentProps) {
   const { chat, messages } = loaderData;
+  const mqttClientRef = useRef<mqtt.MqttClient | null>(null);
+  const localStorageToken = localStorage.getItem('token');
+  const mqttHost = 'wss://mqtt.cipherdolls.com';
+  const clientId = `frontend_${Math.random().toString(16).slice(3)}`;
+
+  useEffect(() => {
+    if (!mqttClientRef.current) {
+      const mqttClient = mqtt.connect(mqttHost, {
+        clientId,
+        username: 'frontend',
+        password: localStorageToken?.replaceAll('"', ''),
+        will: {
+          topic: `connections`,
+          payload: Buffer.from(JSON.stringify({ clientId, deviceType: 'browser', status: 'disconnected' })), // Convert string to Buffer
+          qos: 1,
+          retain: false,
+        },
+      });
+      mqttClientRef.current = mqttClient;
+
+      const userTopic = `chats/${chat.id}/processEvents`;
+      mqttClient.subscribe(userTopic);
+
+      const handleMessage = (topic: string, message: Buffer) => {
+        const processEvent: ProcessEvent = JSON.parse(message.toString());
+        console.log(processEvent);
+        // Handle the event 
+      };
+
+      mqttClient.on('message', handleMessage);
+      mqttClient.on('connect', () => {
+        mqttClient.publish(`connections`, JSON.stringify({ clientId, deviceType: 'browser', status: 'connected' }), { qos: 1 });
+      });
+
+      // Clean up the subscription and event listener on unmount
+      return () => {
+        mqttClient.unsubscribe(userTopic);
+        mqttClient.off('message', handleMessage);
+        mqttClient.end(); // Disconnect the client
+        mqttClientRef.current = null;
+      };
+    }
+    // eslint-disable-next-line
+  }, [chat.id]);
+
+
+
+
 
   return (
     <>
