7aa0c345a9dc5e37b59bde4f9718dcfc8308b3c7
ffaerber <ffaerber@gmail.com>
2025-02-24 16:47:01 +0200

    load avatars and dolls in dashboard

--- app/routes/_main._index.tsx
+++ app/routes/_main._index.tsx
@@ -1,11 +1,49 @@
-import { Link } from 'react-router';
+import { Link, redirect } from 'react-router';
 import { Card } from '~/components/card';
 import DashboardBanner from '~/components/dashboardBanner';
 import { Icons } from '~/components/ui/icons';
+import type { Route } from './+types/_main._index';
+import type { Avatar, Doll } from '~/types';
 
-const Dashbaord = () => {
-  const avatarData = [];
-  const dollsData = [];
+
+
+
+
+export async function clientLoader() {
+  const backendUrl = 'https://api.cipherdolls.com';
+  const localStorageToken = localStorage.getItem('token');
+  if (!localStorageToken) {
+    return redirect('/signin');
+  }
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${localStorageToken?.replaceAll('"', '')}`,
+    },
+  };
+  try {
+    const [avatarsRes, dollsRes] = await Promise.all([
+      fetch(`${backendUrl}/avatars`, headers),
+      fetch(`${backendUrl}/dolls`, headers),
+    ]);
+    if (!avatarsRes.ok || !dollsRes.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const avatars: Avatar[] = await avatarsRes.json();
+    const dolls: Doll[] = await dollsRes.json();
+
+    return { avatars, dolls };
+  } catch (error) {
+    return redirect('/signin');
+  }
+}
+
+
+
+
+export default function Dashbaord({ loaderData }: Route.ComponentProps) {
+
+  const { avatars, dolls } = loaderData;
+
   return (
     <div className='flex flex-col lg:gap-16 md:gap-12 gap-8 flex-1'>
       <div className='flex flex-col sm:gap-4 gap-7'>
@@ -35,7 +73,7 @@ const Dashbaord = () => {
               </Link>
             </Card.Header>
             <Card.Content>
-              {avatarData.length === 0 ? (
+              {avatars.length === 0 ? (
                 <div className='sm:pb-14'>
                   <div className='py-6 sm:py-4 px-6 flex sm:flex-col flex-row items-center sm:justify-center sm:gap-2 gap-6'>
                     <h1 className='sm:text-heading-h1 text-heading-h2'>🎨</h1>
@@ -56,7 +94,7 @@ const Dashbaord = () => {
           <Card.Label>Your Dolls</Card.Label>
           <Card.Main className='max-h-max'>
             <Card.Content>
-              {dollsData.length === 0 ? (
+              {dolls.length === 0 ? (
                 <div className='sm:py-14'>
                   <div className='py-6 sm:py-4 px-6 flex sm:flex-col flex-row items-center sm:justify-center sm:gap-2 gap-6'>
                     <h1 className='sm:text-heading-h1 text-heading-h2'>🤷‍♀️</h1>
@@ -79,4 +117,3 @@ const Dashbaord = () => {
   );
 };
 
-export default Dashbaord;
