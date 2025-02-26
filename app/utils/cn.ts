856e674cf354e6d61bef31ebaddb592bf527139a
levanton <31097602+levanton@users.noreply.github.com>
2025-02-26 14:13:36 +0200

    fix: tailwind text classes conflict

--- app/utils/cn.ts
+++ app/utils/cn.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const customTwMerge = extendTailwindMerge<'text-size'>({
+  extend: {
+    classGroups: {
+      'text-size': ['text-body-sm', 'text-body-md', 'text-body-lg'],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
