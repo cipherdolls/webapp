79cee28301fe02eb59360f614f5f7c5a78f7106c
levanton <31097602+levanton@users.noreply.github.com>
2025-02-27 12:35:17 +0200

    removed: cn customization for classes conflict fix

--- app/utils/cn.ts
+++ app/utils/cn.ts
@@ -1,14 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
-import { extendTailwindMerge } from 'tailwind-merge';
+import { twMerge } from 'tailwind-merge';
 
-const customTwMerge = extendTailwindMerge<'text-size'>({
-  extend: {
-    classGroups: {
-      'text-size': ['text-body-sm', 'text-body-md', 'text-body-lg'],
-    },
-  },
-});
 
 export function cn(...inputs: ClassValue[]) {
-  return customTwMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
