27b938b19d54dfa330d3803d3ddda8620c777144
ffaerber <ffaerber@gmail.com>
2025-03-05 16:14:17 +0200

    fix dashbaord

--- app/components/avatarCard.tsx
+++ app/components/avatarCard.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router';
+import { Form, Link } from 'react-router';
 import { PICTURE_SIZE } from '~/constants';
 import type { Avatar } from '~/types';
 import { getPicture } from '~/utils/getPicture';
@@ -8,15 +8,17 @@ export const AvatarCard = ({ avatar }: { avatar: Avatar }) => {
   const { name, shortDesc, id, chats } = avatar;
 
   return (
-    <Link to={`/avatars/${id}`} className='lg:p-4 sm:p-3.5 p-3 flex items-center justify-between gap-4 sm:gap-4.5 flex-wrap'>
+    <div className='lg:p-4 sm:p-3.5 p-3 flex items-center justify-between gap-4 sm:gap-4.5 flex-wrap'>
       <div className='sm:gap-4.5 gap-4 flex items-center'>
         <div className='shrink-0'>
-          <img
-            src={getPicture(avatar, 'avatars', false, PICTURE_SIZE.avatar)}
-            srcSet={getPicture(avatar, 'avatars', true, PICTURE_SIZE.avatar)}
-            alt={name}
-            className='lg:size-20 sm:size-16 size-14 rounded-full'
-          />
+          <Link to={`/avatars/${id}`} >
+            <img
+              src={getPicture(avatar, 'avatars', false, PICTURE_SIZE.avatar)}
+              srcSet={getPicture(avatar, 'avatars', true, PICTURE_SIZE.avatar)}
+              alt={name}
+              className='lg:size-20 sm:size-16 size-14 rounded-full'
+            />
+          </Link>
         </div>
         <div className='flex flex-1 gap-0.5 flex-col'>
           <h4 className='text-base-black sm:text-heading-h4 text-body-lg font-semibold'>{name}</h4>
@@ -24,15 +26,22 @@ export const AvatarCard = ({ avatar }: { avatar: Avatar }) => {
         </div>
       </div>
 
-      {chats[0]?.id ? (
-        <Button.Root className='sm:px-6 px-5' responsive>
-          Start Chat
-        </Button.Root>
-        ) : (
-        <Button.Root className='sm:px-6 px-5' responsive>
-          Continue Chat
-        </Button.Root>
+      {chats.length > 0 ? (
+        <Link to={`/chats/${avatar.chats[0].id}`}>
+          <Button.Root size='sm' className='px-5'>
+            Continue Chats
+          </Button.Root>
+        </Link>
+      ) : (
+        <Form method='POST' action='/chats' >
+          <input hidden name='avatarId' id='avatarId' value={avatar.id} readOnly />
+          <Button.Root type='submit' size='sm' className='px-5'>
+            Start Chat
+          </Button.Root>
+        </Form>
       )}
-    </Link>
+
+
+    </div>
   );
 };
