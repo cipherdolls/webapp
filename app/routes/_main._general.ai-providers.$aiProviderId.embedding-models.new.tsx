6b24d7045e3b6ed4740cd27ef0453030b4a05c45
cagdas <mcagdasbayirli@gmail.com>
2025-03-13 14:51:07 +0300

    embedding model modal is added

--- app/routes/_main._general.ai-providers.$aiProviderId.embedding-models.new.tsx
+++ app/routes/_main._general.ai-providers.$aiProviderId.embedding-models.new.tsx
@@ -1,8 +1,14 @@
-import { redirect, useFetcher, useRouteLoaderData } from 'react-router';
+import { redirect, useFetcher, useNavigate, useRouteLoaderData } from 'react-router';
 
 import { fetchWithAuth } from '~/utils/fetchWithAuth';
 import type { AiProvider, User } from '~/types';
 import type { Route } from './+types/_main._general.ai-providers.$aiProviderId.embedding-models.new';
+import * as Button from '~/components/ui/button/button';
+import * as Dialog from '@radix-ui/react-dialog';
+import * as Drawer from '~/components/ui/drawer';
+import { Icons } from '~/components/ui/icons';
+import * as Input from '~/components/ui/input/input';
+import * as Checkbox from '@radix-ui/react-checkbox';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'New Embedding Model' }];
@@ -11,19 +17,123 @@ export function meta({}: Route.MetaArgs) {
 export default function aiProviderShow({ loaderData }: Route.ComponentProps) {
   const aiProvider = useRouteLoaderData('routes/_main._general.ai-providers.$aiProviderId') as AiProvider;
   const fetcher = useFetcher();
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    navigate(`/ai-providers/${aiProvider.id}`);
+  };
 
   return (
+    <Drawer.Root
+      defaultOpen
+      onOpenChange={(open) => {
+        if (!open) handleClose();
+      }}
+    >
+      <Drawer.Content>
+        <Drawer.Title>Add New Embedding Model for {aiProvider.name}</Drawer.Title>
+        <fetcher.Form method='POST' action='/chat-models/new' className='size-full flex flex-col'>
+          <Drawer.Body className='flex flex-col gap-3'>
+            <input type='hidden' name='aiProviderId' value={aiProvider.id} />
 
-    <fetcher.Form method='POST' action="/embedding-models/new">
-      <input name='aiProviderId' value={aiProvider.id}  />
-      <input name='name' />
-      <input name='providerModelName' />
-      <input name='dollarPerInputToken' />
-      <input name='dollarPerOutputToken' />
-      <input name='contextWindow' />
-      <input name='recommended' />
-      <button type='submit'>save</button>
-    </fetcher.Form>
+            <Input.Root>
+              <Input.Label id='name' htmlFor='name'>
+                Embedding Model Name
+              </Input.Label>
+              <Input.Input
+                className='text-base-black border border-neutral-04 py-3.5 px-3'
+                id='name'
+                name='name'
+                type='text'
+                placeholder='GPT-4.5'
+              />
+            </Input.Root>
 
+            <Input.Root>
+              <Input.Label id='providerModelName' htmlFor='providerModelName'>
+                Provider Model Name
+              </Input.Label>
+              <Input.Input
+                className='text-base-black border border-neutral-04 py-3.5 px-3'
+                id='providerModelName'
+                name='providerModelName'
+                type='text'
+                placeholder='Deepseek'
+              />
+            </Input.Root>
+
+            <div className='grid grid-cols-1 sm:grid-cols-2 gap-3'>
+              <Input.Root>
+                <Input.Label id='dollarPerInputToken' htmlFor='dollarPerInputToken'>
+                  $ per Input Token
+                </Input.Label>
+                <Input.Input
+                  className='text-base-black border border-neutral-04 py-3.5 px-3'
+                  id='dollarPerInputToken'
+                  name='dollarPerInputToken'
+                  type='number'
+                  placeholder='0.0001'
+                  step='any'
+                />
+              </Input.Root>
+
+              <Input.Root>
+                <Input.Label id='dollarPerOutputToken' htmlFor='dollarPerOutputToken'>
+                  $ per Output Token
+                </Input.Label>
+                <Input.Input
+                  className='text-base-black border border-neutral-04 py-3.5 px-3'
+                  id='dollarPerOutputToken'
+                  name='dollarPerOutputToken'
+                  type='number'
+                  placeholder='0.0001'
+                  step='any'
+                />
+              </Input.Root>
+            </div>
+            <Input.Root>
+              <Input.Label id='contextWindow' htmlFor='contextWindow'>
+                Context Window
+              </Input.Label>
+              <Input.Input
+                className='text-base-black border border-neutral-04 py-3.5 px-3'
+                id='contextWindow'
+                name='contextWindow'
+                type='number'
+                placeholder='8192'
+              />
+            </Input.Root>
+
+            <div className='flex items-center gap-2'>
+              <Checkbox.Root
+                className='flex size-4.5 appearance-none items-center justify-center rounded-full border border-neutral-03 data-[state=checked]:bg-base-black bg-transparent outline-none focus:shadow-neutral-02'
+                id='recommended'
+                name='recommended'
+              >
+                <Checkbox.Indicator>
+                  <Icons.check className='text-white size-4.5' />
+                </Checkbox.Indicator>
+              </Checkbox.Root>
+              <label className='text-body-sm font-semibold text-neutral-01' htmlFor='recommended'>
+                Recommended
+              </label>
+            </div>
+          </Drawer.Body>
+          <Drawer.Footer>
+            <Button.Root type='submit' className='w-full'>
+              Save
+            </Button.Root>
+          </Drawer.Footer>
+        </fetcher.Form>
+        <Dialog.Close asChild>
+          <button
+            className='absolute focus:outline-none -left-[78px] top-4.5 size-10 bg-white rounded-full flex items-center justify-center z-10'
+            aria-label='Close'
+          >
+            <Icons.close />
+          </button>
+        </Dialog.Close>
+      </Drawer.Content>
+    </Drawer.Root>
   );
 }
