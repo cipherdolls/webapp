2be2ae22f09c89f4f6142fcd9b416ace1efb5d66
levanton <31097602+levanton@users.noreply.github.com>
2025-04-21 19:49:18 +0300

    fix: go back chat button

--- app/routes/_main.chats._index.tsx
+++ app/routes/_main.chats._index.tsx
@@ -1,7 +1,9 @@
 import ChatWelcome from '~/components/chat/ChatWelcome';
 import { fetchWithAuth } from '~/utils/fetchWithAuth';
 import type { Route } from './+types/_main.chats._index';
-import { redirect } from 'react-router';
+import { useMediaQuery } from 'usehooks-ts';
+import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
 
 export async function clientLoader() {
   const [chats, avatars] = await Promise.all([
@@ -9,15 +11,20 @@ export async function clientLoader() {
     fetchWithAuth('avatars').then((res) => res.json()),
   ]);
 
-  if (chats.length > 0) {
-    return redirect(`/chats/${chats[0].id}`);
-  }
-
   return { chats, avatars };
 }
 
 export default function ChatsIndex({ loaderData }: Route.ComponentProps) {
   const { chats, avatars } = loaderData;
+  const isMobile = useMediaQuery('(min-width: 768px)');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (chats.length > 0 && isMobile) {
+      const firstChat = chats[0];
+      navigate(`/chats/${firstChat.id}`);
+    }
+  }, [chats, isMobile, navigate]);
 
   return <ChatWelcome chats={chats} avatars={avatars} />;
 }
