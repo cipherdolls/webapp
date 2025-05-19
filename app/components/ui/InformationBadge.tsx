e7e3c02b134c7bfb287720e0f4166c3c9ef2913f
cagdas <mcagdasbayirli@gmail.com>
2025-05-19 14:48:40 +0300

    dolls tooltip position and text is fixed

--- app/components/ui/InformationBadge.tsx
+++ app/components/ui/InformationBadge.tsx
@@ -5,14 +5,16 @@ type InformationBadgeProps = {
   dontShow?: boolean;
   tooltipText?: string;
   className?: string;
+  side?: 'right' | 'left' | 'top' | 'bottom';
 };
 
 export const InformationBadge = ({
   dontShow,
   tooltipText = 'High-speed AI for real-time apps and chatbots.',
   className = 'h-4 w-4 ',
+  side = 'right',
 }: InformationBadgeProps) => {
   if (dontShow) return null;
 
-  return <Tooltip side='right' trigger={<Icons.information className={`${className} text-base-black`} />} content={tooltipText} />;
+  return <Tooltip side={side} trigger={<Icons.information className={`${className} text-base-black`} />} content={tooltipText} />;
 };
