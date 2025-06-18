7bf6d3cad58d11a3d1f39153bac305480f2ac1e9
David <grikurovdavid@gmail.com>
2025-06-18 23:00:49 +0200

    fix bug with modal

--- app/components/howItWorksModal.tsx
+++ app/components/howItWorksModal.tsx
@@ -6,10 +6,10 @@ const HowItWorksModal = () => {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className='underline text-body-md'>How It Works</button>
+        <button className='underline text-neutral-01 text-body-md'>How It Works</button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className='sm:bg-transparent bg-neutral-02 fixed inset-0 pointer-events-none z-20'>
+        <Dialog.Overlay className='sm:bg-transparent bg-neutral-02 fixed inset-0 pointer-events-none z-40'>
           <div
             className='absolute left-1/2 -translate-x-1/2
         w-[375px] h-[514px] sm:w-[480px] sm:h-[538px] 
@@ -30,7 +30,7 @@ const HowItWorksModal = () => {
           />
         </Dialog.Overlay>
 
-        <Dialog.Content className='fixed left-1/2 bottom-0 sm:bottom-auto sm:top-1/2 -translate-x-1/2 sm:-translate-y-1/2 focus:outline-none max-w-[480px] bg-gradient-1 w-full rounded-xl sm:py-8 py-9 sm:px-12 px-4.5 shadow-bottom backdrop-blur-lg z-20'>
+        <Dialog.Content className='fixed left-1/2 bottom-0 sm:bottom-auto sm:top-1/2 -translate-x-1/2 sm:-translate-y-1/2 focus:outline-none max-w-[480px] bg-gradient-1 w-full rounded-xl sm:py-8 py-9 sm:px-12 px-4.5 shadow-bottom backdrop-blur-lg z-50'>
           <div className='absolute top-3 left-1/2 -translate-x-1/2 bg-neutral-03 rounded-full w-16 h-1 sm:hidden' />
           <div className='flex flex-col sm:gap-4.5 gap-3'>
             <div className='sm:text-heading-h1 text-heading-h2 text-center'>🥷</div>
