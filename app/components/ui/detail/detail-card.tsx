ba48e2916d98e2c033a1baa6a3b029f5a7afe0a5
cagdas <mcagdasbayirli@gmail.com>
2025-06-18 16:33:27 +0300

    scenario detail page is refactored

--- app/components/ui/detail/detail-card.tsx
+++ app/components/ui/detail/detail-card.tsx
@@ -10,6 +10,7 @@ const DetailCard = ({
   withPrice,
   copy,
   copyText = '',
+  isScenario,
 }: {
   children: ReactNode;
   className?: string;
@@ -17,13 +18,38 @@ const DetailCard = ({
   withPrice?: boolean;
   copy?: boolean;
   copyText?: string;
+  isScenario?: boolean;
 }) => {
   const { copied, copyToClipboard } = useCopyToClipboard();
 
+  if (isScenario) {
+    return (
+      <div className={cn('bg-gradient-1 shadow-regular rounded-xl px-5 py-[18px] max-h-max relative', className)}>
+        {title && (
+          <div className='flex items-center justify-between mb-4'>
+            {title && <h2 className='text-body-md font-semibold text-gray-800'>{title}</h2>}
+            {copy && (
+              <button
+                onClick={() => copyToClipboard(copyText)}
+                className='bg-transparent hover:bg-neutral-05 rounded-full transition-colors'
+              >
+                {copied ? <Icons.copied /> : <Icons.copy />}
+              </button>
+            )}
+          </div>
+        )}
+        {children}
+        {withPrice && (
+          <span className='text-xs text-neutral-01 font-semibold flex items-center justify-end mt-3'>Prices are per million token</span>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div className={cn('bg-white shadow-regular rounded-xl px-5 py-[18px] max-h-max relative', className)}>
-      <div className='flex items-center justify-between'>
-        {title && <h2 className='text-body-md font-semibold mb-4 text-gray-800'>{title}</h2>}
+      <div className='flex items-center justify-between mb-4'>
+        {title && <h2 className='text-body-md font-semibold text-gray-800'>{title}</h2>}
         {copy && (
           <button onClick={() => copyToClipboard(copyText)} className='bg-transparent hover:bg-neutral-05 rounded-full transition-colors'>
             {copied ? <Icons.copied /> : <Icons.copy />}
