0c68448ffb13576e694038f71c14a4b955589a9a
David <grikurovdavid@gmail.com>
2025-07-08 20:15:40 +0200

    441 swap icon

--- app/components/ui/icons.tsx
+++ app/components/ui/icons.tsx
@@ -579,7 +579,7 @@ export const Icons = {
       <path d='M9 9 3 3' />
     </svg>
   ),
-  avatars: () => (
+  scenarios: () => (
     <svg
       xmlns='http://www.w3.org/2000/svg'
       width='20'
