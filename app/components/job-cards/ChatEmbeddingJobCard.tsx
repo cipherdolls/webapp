788075af5661069483aa5299185de444e6aa85b0
David <grikurovdavid@gmail.com>
2025-07-15 18:39:46 +0200

    added error tooltips for scenario and related services

--- app/components/job-cards/ChatEmbeddingJobCard.tsx
+++ app/components/job-cards/ChatEmbeddingJobCard.tsx
@@ -2,13 +2,25 @@ import { DataCard } from '~/components/DataCard';
 import type { Message } from '~/types';
 import { formatModelName } from '~/utils/formatModelName';
 import { scientificNumConvert } from '~/utils/scientificNumConvert';
+import Tooltip from '~/components/ui/tooltip';
+import { Icons } from '~/components/ui/icons';
+import React from 'react';
 
 const ChatEmbeddingJobCard = ({ message }: { message: Message }) => {
   const embeddingJob = message?.embeddingJob;
 
   return (
     <DataCard.Root>
-      <DataCard.Label>Embedding Job</DataCard.Label>
+      <div className='flex items-center gap-2'>
+        <Tooltip
+          side={'top'}
+          trigger={<Icons.warning className='size-5 text-specials-danger mb-4' />}
+          content={embeddingJob.embeddingModel.error}
+          popoverClassName='max-w-[350px]'
+        />
+        <DataCard.Label>Embedding Job</DataCard.Label>
+      </div>
+
       <DataCard.Wrapper>
         <DataCard.ItemDataGrid
           data={[
