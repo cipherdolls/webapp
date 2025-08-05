f155b839cc11c17acd9ecf8f8a10c0a15a19a722
Anton Lev <antonlev@Antons-Mac-mini.local>
2025-08-05 14:00:08 +0300

    feat: update user with tunstack

--- app/routes/_main._general.account.tsx
+++ app/routes/_main._general.account.tsx
@@ -1,4 +1,4 @@
-import { useFetcher, useRouteLoaderData } from 'react-router';
+import {  useRouteLoaderData } from 'react-router';
 import type { Route } from './+types/_main._general.account';
 import type { User } from '~/types';
 import { Icons } from '~/components/ui/icons';
@@ -6,36 +6,24 @@ import YourInfo from '~/components/yourInfo';
 import AccountInfoCard from '~/components/account-info-card';
 import YourWalletModal from '~/components/yourWalletModal';
 import ApiKeyModal from '~/components/apiKeyModal';
-import { fetchWithAuth } from '~/utils/fetchWithAuth';
 import { formatEther } from 'ethers';
 import SignOutModal from '~/components/signOutModal';
 import { Tooltip } from '~/components/ui/tooltip';
 import OP from '~/assets/svg/op-png.png';
 import LogoSvg from '~/assets/svg/logo.svg';
+import { useUser } from '~/hooks/queries/userQueries';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Account' }];
 }
 
-export async function clientAction({ request }: Route.ClientActionArgs) {
-  const formData = await request.formData();
-  const userId = formData.get('userId');
-  const jsonData: Record<string, any> = {};
-  formData.forEach((value, key) => {
-    jsonData[key] = value;
-  });
-  const res = await fetchWithAuth(`users/${userId}`, {
-    method: request.method,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(jsonData),
-  });
-  return await res.json();
-}
-
 export default function Account({}: Route.ComponentProps) {
-  const me = useRouteLoaderData('routes/_main') as User;
-  const { id, signerAddress, weiBalance, freeWeiBalance } = me;
-  const fetcher = useFetcher();
+  const { data: user } = useUser();
+  const me = user;
+  if (!me) {  
+    return null;
+  }
+  const { weiBalance, freeWeiBalance } = me;
 
   return (
     <div className='flex flex-col lg:gap-16 md:gap-12 gap-8 flex-1'>
@@ -88,7 +76,9 @@ export default function Account({}: Route.ComponentProps) {
                     </div>
                   </button>
                   <h2 className='text-heading-h2 sm:text-heading-h1 font-semibold break-all'>
-                    {!freeWeiBalance || Number(formatEther(freeWeiBalance)) === 0 ? '0.00' : String(Number(formatEther(freeWeiBalance)).toFixed(5))}{' '}
+                    {!freeWeiBalance || Number(formatEther(freeWeiBalance)) === 0
+                      ? '0.00'
+                      : String(Number(formatEther(freeWeiBalance)).toFixed(5))}{' '}
                     <span className='text-neutral-01'>ETH</span>
                   </h2>
                 </div>
@@ -102,11 +92,10 @@ export default function Account({}: Route.ComponentProps) {
         </div>
       </div>
       <div className='flex sm:flex-row flex-col-reverse sm:gap-0 gap-8 sm:flex-1 sm:divide-x divide-neutral-04 pb-2.5'>
-        <fetcher.Form method='PATCH' className='w-full'>
-          <input name='userId' value={id} hidden readOnly />
-          <input name='signerAddress' value={signerAddress} hidden readOnly />
-          <YourInfo me={me} fetcher={fetcher} />
-        </fetcher.Form>
+        
+        
+          <YourInfo me={me} />
+        
 
         <div className='sm:pl-4 sm:max-w-[352px] w-full flex flex-col sm:gap-10 gap-8'>
           <AccountInfoCard
