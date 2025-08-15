0280769066c252e3608fe525e8b6399cbb1b7a61
davidgrii <grikurovdavid@gmail.com>
2025-08-15 17:37:20 +0200

    tooltip fix

--- app/components/ui/tooltip.tsx
+++ app/components/ui/tooltip.tsx
@@ -189,7 +189,7 @@ const DesktopView = ({
             side={side}
             align={align}
             className={cn(
-              'z-50 overflow-hidden break-words backdrop-blur-xl rounded-[10px] bg-neutral-03 px-3 py-2 font-semibold text-label text-base-black animate-tooltip fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+              'z-50 overflow-hidden break-words backdrop-blur-xl rounded-[10px] bg-neutral-03 px-3 py-2 font-semibold text-label text-base-black animate-tooltip-toggle fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
               className
             )}
             sideOffset={5}
