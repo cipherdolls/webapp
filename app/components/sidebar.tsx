0f6aa0b99c036fda29343cc9eb0368b45b60826f
davidgrii <grikurovdavid@gmail.com>
2025-08-30 11:56:39 +0200

    updated static paths with ROUTES config

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -55,7 +55,7 @@ const Sidebar = ({ className }: { className?: string }) => {
   return (
     <aside className={cn('sm:w-[104px] flex', className)}>
       <div className='sm:py-4 sm:px-2 flex flex-col items-center justify-between flex-1 sm:bg-transparent sm:rounded-none rounded-t-xl bg-[linear-gradient(86.23deg,rgba(254,253,248,0.48)_0%,rgba(255,255,255,0.48)_100%)] sm:bg-none'>
-        <Link to={ROUTES.account} className='py-3.5 sm:block hidden'>
+        <Link to={ROUTES.chats} className='py-3.5 sm:block hidden'>
           <Icons.iconLogo className={cn(isAdmin ? 'text-specials-danger' : 'text-base-black')} />
         </Link>
         <div className='flex sm:flex-col flex-row sm:gap-3 w-full sm:justify-start justify-around sm:py-0 py-1'>
