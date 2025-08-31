4560798d83f93cf8d19974905ea9743ca3b4ffdc
davidgrii <grikurovdavid@gmail.com>
2025-08-31 23:03:02 +0200

    updated ai services skeletons

--- app/components/ai-services/UniversalModelTab.tsx
+++ app/components/ai-services/UniversalModelTab.tsx
@@ -32,10 +32,18 @@ function ModelSkeleton({ count = 3 }: { count?: number }) {
     <div className='flex flex-col gap-10 pb-5'>
       {Array.from({ length: count }).map((_, i) => (
         <div key={i} className='flex flex-col gap-4'>
-          <div className='rounded-[10px] h-6 bg-gradient-1 w-full max-w-[200px] animate-pulse'></div>
           <div className='flex flex-col gap-3'>
-            <div className='rounded-[10px] h-[110px] bg-gradient-1 w-full animate-pulse'></div>
-            <div className='rounded-[10px] h-[110px] bg-gradient-1 w-full animate-pulse'></div>
+            <div className='flex justify-between items-center mb-1'>
+              <div className='flex gap-2 items-center'>
+                <div className='w-6 h-6 rounded-lg bg-neutral-04'/>
+                <div className='w-32 h-6 rounded-lg bg-neutral-04'/>
+              </div>
+
+              <Icons.more className='text-pink-01 group-hover:text-base-black transition-colors' />
+            </div>
+
+            <div className='rounded-xl h-[110px] bg-neutral-04 w-full animate-pulse'></div>
+            <div className='rounded-xl h-[110px] bg-neutral-04 w-full animate-pulse'></div>
           </div>
         </div>
       ))}
@@ -122,7 +130,7 @@ export function UniversalModelTab({ tabType, data, ...queryProps }: UniversalMod
       )}
 
       {/* Results Section */}
-      {queryProps.isLoading ? (
+      {!queryProps.isLoading ? (
         <ModelSkeleton />
       ) : (
         <div className='flex flex-col gap-10 pb-5'>
